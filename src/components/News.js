e4aa3f20258a576540d29b30300f09a590d4b9a1
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-02-07 01:25:35 +0500

    Fetching API Key from API News

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,20 @@ import NewsItems from './NewsItems'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news Component
-        <NewsItems/>
+      <div className='container my-3'>
+        <h2>NewsApp - Top Headlines</h2>
+        <div className='row'>
+            <div className='col-md-4'>
+                <NewsItems title="myTiltle" description="myDescription"/>
+            </div>
+            <div className='col-md-4'>
+                <NewsItems title="myTiltle" description="myDescription"/>
+            </div>
+            <div className='col-md-4'>
+                <NewsItems title="myTiltle" description="myDescription"/>
+            </div>
+        </div>
+    
       </div>
     )
   }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
   render() {
+    let {title, description} = this.props;
     return (
       <div>
-        This is news Item components
+        <div  className="card" style={{width: "18rem"}}>
+        <img src= "https://m.files.bbci.co.uk/modules/bbc-morph-news-waf-page-meta/5.3.0/bbc_news_logo.png"  className="card-img-top" alt="..."/>
+        <div  className="card-body">
+            <h5  className="card-title">{title}</h5>
+            <p  className="card-text">{description}</p>
+            <a href="/"  className="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
       </div>
     )
   }
