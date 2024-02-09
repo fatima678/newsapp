2830d85b3b20ab8839e9d8a37cda2f0856730ad6
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-02-09 18:31:04 +0500

    Display 5 NewsItems per page using variable

---
+++ src/components/Spinner.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react'
+// import loadingspinner from './loadingspinner.gif'
+
+// export class Spinner extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <img src={loadingspinner} alt='loadingspinner'></img>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Spinner
+// import React from 'react';
+// import loader from '../loader.gif'; 
+
+// const Spinner = () => {
+//   return (
+//     <div className="text-center">
+//       <img src={loader} alt="Loading..." />
+//     </div>
+//   );
+// };
+
+// export default Spinner;
