4acad1e8626b775324e54811af44e4e624da147b
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-02-10 23:52:04 +0500

    Fetching news categories

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -5,7 +5,7 @@ export class NewsItems extends Component {
     let {title, description, imageUrl, url} = this.props;
     return (
       <div className='my-3'>
-        <div  className="card" style={{width: "18rem"}}>
+        <div  className="card">
         <img src= {!imageUrl?"https://i.abcnewsfe.com/a/646bf65a-6db9-4d9e-be2f-ad4db1cd7ee6/killer-whales-trapped-nc-jt-240206_1707259991926_hpMain_16x9.jpg?w":imageUrl}  className="card-img-top" alt="..."/>
         <div  className="card-body">
             <h5  className="card-title">{title}...</h5>
