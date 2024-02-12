7ae1c06434979eb205e9d370ed34ec6c6ce60b9b
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-02-12 21:57:21 +0500

    Adding a top loading bar in Newsapp

--- src/App.js
+++ src/App.js
@@ -1,68 +1,79 @@
-
-// import './App.css';
-// import React, { Component } from 'react'
+// import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Navbar from './components/Navbar';
 // import News from './components/News';
-// // import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+// import React, { useState } from 'react'
+// import LoadingBar from 'react-top-loading-bar';
+// import progress from 'react-top-loading-bar'
 
 // export default class App extends Component {
+//   // variable for chnaging pagesize
+//   pageSize=10;
 //   render() {
 //     return (
-//       <>
 //       <div>
 //         <Router>
-//         <Navbar/>
-//         <Switch> 
-//           {/* <Route><News pageSize={6} country="in" category="science"/></Route> */}
-//           <Route path='/'><News pageSize={6} country="in" category="general"/></Route>
-//           <Route path='/business'><News pageSize={6} country="in" category="business"/></Route>
-//           <Route path='/entertainment'><News pageSize={6} country="in" category="entertainment"/></Route>
-//           <Route path='/general'><News pageSize={6} country="in" category="general"/></Route>
-//           <Route path='/health'><News pageSize={6} country="in" category="health"/></Route>
-//           <Route path='/science'><News pageSize={6} country="in" category="science"/></Route>
-//           <Route path='/sports'><News pageSize={6} country="in" category="sports"/></Route>
-//           <Route path='/technology'><News pageSize={6} country="in" category="technology"/></Route>
-
-        
-//         </Switch>
-         
-          
-        
+//           <Navbar />
+//           <LoadingBar
+//         color='#f11946'
+//         progress={progress}
+//         // onLoaderFinished={() => setProgress(0)}
+//       />
+//           <Routes>
+//             <Route exact path='/' element={<News key='general' pageSize={this.pageSize} country="in" category="General" />} />
+//             <Route exact  path='/business' element={<News key='business' pageSize={this.pageSize} country="in" category="Business" />} />
+//             <Route exact path='/entertainment' element={<News key='entertainment' pageSize={this.pageSize} country="in" category="Entertainment" />} />
+//             <Route exact path='/general' element={<News key='' pageSize={this.pageSize} country="in" category="General" />} />
+//             <Route exact path='/health' element={<News  key='health'pageSize={this.pageSize} country="in" category="Health" />} />
+//             <Route exact path='/science' element={<News key='science' pageSize={this.pageSize} country="in" category="Science" />} />
+//             <Route exact path='/sports' element={<News key='sports' pageSize={this.pageSize} country="in" category="Sports" />} />
+           
+//           </Routes>
 //         </Router>
-        
 //       </div>
-//       </>
-//     )
-    
+//     );
 //   }
 // }
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import News from './components/News';
-
+import LoadingBar from 'react-top-loading-bar';
+import progress from 'react-top-loading-bar'
 export default class App extends Component {
-  // variable for chnaging pagesize
-  pageSize=10;
+  // variable for changing pagesize
+  pageSize = 10;
+  state={
+    progress:0
+  }
+  
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
+  
   render() {
     return (
       <div>
         <Router>
           <Navbar />
+          <LoadingBar
+            color='#bf1408'
+            height={3}
+            progress={100} 
+          />
           <Routes>
-            <Route exact path='/' element={<News key='general' pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact  path='/business' element={<News key='business' pageSize={this.pageSize} country="in" category="business" />} />
-            <Route exact path='/entertainment' element={<News key='entertainment' pageSize={this.pageSize} country="in" category="entertainment" />} />
-            <Route exact path='/general' element={<News key='' pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact path='/health' element={<News  key='health'pageSize={this.pageSize} country="in" category="health" />} />
-            <Route exact path='/science' element={<News key='science' pageSize={this.pageSize} country="in" category="science" />} />
-            <Route exact path='/sports' element={<News key='sports' pageSize={this.pageSize} country="in" category="sports" />} />
-           
+            <Route exact path='/' element={<News  setProgress={this.setProgress} key='general' pageSize={this.pageSize} country="in" category="General" />} />
+            <Route exact path='/business' element={<News  setProgress={this.setProgress} key='business' pageSize={this.pageSize} country="in" category="Business" />} />
+            <Route exact path='/entertainment' element={<News  setProgress={this.setProgress} key='entertainment' pageSize={this.pageSize} country="in" category="Entertainment" />} />
+            <Route exact path='/general' element={<News setProgress={this.setProgress}  key='' pageSize={this.pageSize} country="in" category="General" />} />
+            <Route exact path='/health' element={<News setProgress={this.setProgress}  key='health' pageSize={this.pageSize} country="in" category="Health" />} />
+            <Route exact path='/science' element={<News  setProgress={this.setProgress} key='science' pageSize={this.pageSize} country="in" category="Science" />} />
+            <Route exact path='/sports' element={<News  key='sports' pageSize={this.pageSize} country="in" category="Sports" />} />
           </Routes>
         </Router>
       </div>
     );
   }
 }
+
 
--- src/components/News.js
+++ src/components/News.js
@@ -15,27 +15,37 @@ export class News extends Component {
     category:PropTypes.string,
   }
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     console.log("i am a constructor from News component");
     this.state={
       articles:[],
       loading:false,
       page:1
     }
+    document.title=`${this.props.category} - NewsApp`;
   }
+
+async UpdateNews(){
+  this.props.setProgress(10);
+  const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b22077b39bad439abf1ce3758fa4d1fc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  this.setState({loading: true});
+  let data= await fetch(url);
+  this.props.setProgress(30)
+  let parsedData= await data.json()
+  this.props.setProgress(50)
+  console.log(parsedData);
+  this.setState({articles: parsedData.articles,totalResuts:parsedData.totalResuts,loadinga:false})
+  this.props.setProgress(100);
+
+}
+
+
 // it is a lifecycle method, it will run after render method
 // This line uses the fetch() function to make an HTTP GET request to the specified URL.
 // The await keyword is used to wait for the promise returned by fetch() to resolve. 
 // When resolved, data will contain the response from the server.
-async UpdateNews(){
-  const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b22077b39bad439abf1ce3758fa4d1fc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-  let data= await fetch(url);
-  let parsedData= await data.json()
-  console.log(parsedData);
-  this.setState({articles: parsedData.articles,totalResuts:parsedData.totalResuts,loadinga:false})
 
-}
   async componentDidMount(){
     this.UpdateNews();
   }
@@ -52,7 +62,8 @@ async UpdateNews(){
   render() {
     return (
       <div className='container my-3'>
-      <h2 className='text-center'>NewsApp - Top Headlines</h2>
+      <h2 className='text-center'>NewsApp - Top {this.props.category} headlines</h2>
+      
     
           <div className='row'>
              {/* iterate articles , map is an array method,  jb b map use krty hn tu elements ko unique key deni prti ha */}
