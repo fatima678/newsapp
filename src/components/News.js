1600824b390755651a316d6a3beb8dbbb7126f5b
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-02-13 23:59:04 +0500

    changing class base componet to function base of news component

--- src/components/News.js
+++ src/components/News.js
@@ -1,89 +1,78 @@
-import React, { Component } from 'react'
+// 
+import React, { useEffect , useState} from 'react'
 import NewsItems from './NewsItems'
 import PropTypes from 'prop-types'
 
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-export class News extends Component {
-  static defaultProps={
-    country:'in',
-    pageSize:8,
-    category:'general'
+  const UpdateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=b22077b39bad439abf1ce3758fa4d1fc&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(50);
+    console.log(parsedData);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  };
+
+  useEffect(() => {
+    UpdateNews();
+  }, []);
+
+  const handlePrevClick = async () => {
+    setPage(page - 1);
+    UpdateNews();
   }
-  static propTypes={
-    country:PropTypes.string,
-    pageSize:PropTypes.number,
-    category:PropTypes.string,
+
+  const handleNextClick = async () => {
+    setPage(page + 1);
+    UpdateNews();
   }
 
-  constructor(props){
-    super(props);
-    console.log("i am a constructor from News component");
-    this.state={
-      articles:[],
-      loading:false,
-      page:1
-    }
-    document.title=`${this.props.category} - NewsApp`;
-  }
-
-async UpdateNews(){
-  this.props.setProgress(10);
-  const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b22077b39bad439abf1ce3758fa4d1fc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-  this.setState({loading: true});
-  let data= await fetch(url);
-  this.props.setProgress(30)
-  let parsedData= await data.json()
-  this.props.setProgress(50)
-  console.log(parsedData);
-  this.setState({articles: parsedData.articles,totalResuts:parsedData.totalResuts,loadinga:false})
-  this.props.setProgress(100);
-
-}
-
-
-// it is a lifecycle method, it will run after render method
-// This line uses the fetch() function to make an HTTP GET request to the specified URL.
-// The await keyword is used to wait for the promise returned by fetch() to resolve. 
-// When resolved, data will contain the response from the server.
-
-  async componentDidMount(){
-    this.UpdateNews();
-  }
-    handlePrevClick= async ()=>{
-    this.setState({page: this.state.page - 1})
-    this.UpdateNews();
-
-  }
-    handleNextClick= async ()=>{
-    this.setState({page: this.state.page+ 1})
-    this.UpdateNews();
-  }
-  
-  render() {
-    return (
-      <div className='container my-3'>
-      <h2 className='text-center'>NewsApp - Top {this.props.category} headlines</h2>
-      
-    
-          <div className='row'>
-             {/* iterate articles , map is an array method,  jb b map use krty hn tu elements ko unique key deni prti ha */}
-             {this.state.articles.map((element)=>{        
-              return <div className='col-md-4' key={element.url}> 
-              <NewsItems  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} 
-                url={element.url} author={element.author} date={element.publishedAt}/>
-                {/* we use slice method to limit the character in title and in description */}
-            </div>
-
-             })
-         }     
-        </div>
-        <div className='container d-flex justify-content-between'>
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-        <button  disabled={this.state.page +1 > Math.ceil(this.state.totalResuts/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
+  return (
+    <div className='container my-3'>
+      <h2 className='text-center'>NewsApp - Top {props.category} headlines</h2>
+      <div className='row'>
+        {articles.map((element) => (
+          <div className='col-md-4' key={element.url}> 
+            <NewsItems
+              title={element.title ? element.title.slice(0, 45) : ""}
+              description={element.description ? element.description.slice(0, 88) : ""}
+              imageUrl={element.urlToImage}
+              url={element.url}
+              author={element.author}
+              date={element.publishedAt}
+            />
+          </div>
+        ))}
       </div>
-    )
-  }
+      <div className='container d-flex justify-content-between'>
+        <button disabled={page <= 1} type="button" className="btn btn-dark" onClick={handlePrevClick}> &larr; Previous</button>
+        <button disabled={page + 1 > Math.ceil(totalResults / props.pageSize)} type="button" className="btn btn-dark" onClick={handleNextClick}>Next &rarr;</button>
+      </div>
+    </div>
+  );
 }
 
-export default News
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: 'general'
+}
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+}
+
+export default News;
